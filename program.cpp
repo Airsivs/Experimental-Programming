d5c15ff567ca820525cc66d606380668a190ec5d
Airsivs <mkkovacevick@gmail.com>
2026-07-21 11:50:13 +0200

    Initial commit: Setting up C++ learning environment

---
+++ program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <chrono>
+
+int main(){
+    double pi {3.14};
+    double gravity {6.12};
+    double phi {1.61};
+    double input {};
+    std::cout << pi << '\n';
+    std::cin >> input;
+
+    return 0;
+};
